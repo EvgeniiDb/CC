ee504b865019a572be0d1939bf0db2c39ce8f385
EvgeniiDb <doktor-dell@mail.ru>
2023-02-16 22:38:28 +0300

    1

---
+++ CC/CC/App/CCApp.swift
@@ -0,0 +1,17 @@
+//
+//  CCApp.swift
+//  CC
+//
+//  Created by Евгений Доброволец on 16.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct CCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
